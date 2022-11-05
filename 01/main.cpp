c666d0ddb6e9ab953fd752911a84115282e552f9
R <R@R>
2022-11-05 14:15:06 +0100

    Initial commit

---
+++ 01/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class MainClass
+{
+private:
+    /* data */
+public:
+    MainClass(/* args */);
+    ~MainClass();
+};
+
+MainClass::MainClass(/* args */)
+{
+    cout << "Hellow 01 \n";
+}
+
+MainClass::~MainClass()
+{
+}
+
+// int main(int, char**) {
+//     return 0;
+// }
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include "01/main.cpp"
+
+int main(int, char**) {
+    MainClass mc;
+    return 0;
+}
